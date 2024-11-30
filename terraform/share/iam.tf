fe40a38c2db74020a7daee712c49554265cb2ec8
kyeongjun-dev <kyeongjun.dev@gmail.com>
2024-11-30 11:38:16 +0900

    iam.tf 파일 들여쓰기 수정, ecr.tf 파일 추가 - springboot ECR 추가

--- terraform/share/iam.tf
+++ terraform/share/iam.tf
@@ -3,9 +3,9 @@
 #####
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "EKSClusterRole"
+    name = "EKSClusterRole"
 
-  assume_role_policy = <<POLICY
+    assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -22,20 +22,20 @@ POLICY
 }
 
 output "eks_cluster_role_arn" {
-  value       = "${aws_iam_role.eks_cluster_role.arn}"
+    value       = "${aws_iam_role.eks_cluster_role.arn}"
 }
 
 
 resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    role       = aws_iam_role.eks_cluster_role.name
 }
 
 # Optionally, enable Security Groups for Pods
 # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks_cluster_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+    role       = aws_iam_role.eks_cluster_role.name
 }
 
 #####
@@ -43,35 +43,35 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 #####
 
 resource "aws_iam_role" "eks_nodegroup_role" {
-  name = "EKSNodegroupRole"
+    name = "EKSNodegroupRole"
 
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
+    assume_role_policy = jsonencode({
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            }
+        }]
+        Version = "2012-10-17"
+    })
 }
 
 output "eks_nodegroup_role_arn" {
-  value       = "${aws_iam_role.eks_nodegroup_role.arn}"
+    value       = "${aws_iam_role.eks_nodegroup_role.arn}"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_nodegroup_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+    role       = aws_iam_role.eks_nodegroup_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_nodegroup_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+    role       = aws_iam_role.eks_nodegroup_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_nodegroup_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    role       = aws_iam_role.eks_nodegroup_role.name
 }
