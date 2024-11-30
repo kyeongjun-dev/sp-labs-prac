ae1bd489e389e1bee83f8808c9af36699d507b40
kyeongjun-dev <kyeongjun.dev@gmail.com>
2024-11-30 16:11:16 +0900

    vpc.tf 파일에 vpc id, subnet id의 output 추가

--- terraform/share/vpc.tf
+++ terraform/share/vpc.tf
@@ -19,3 +19,19 @@ module "vpc" {
         Creator = "kyeongjun.dev"
     }
 }
+
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
+
+output "public_subnets" {
+  value = "${module.vpc.public_subnets}"
+}
+
+output "private_subnets" {
+  value = "${module.vpc.private_subnets}"
+}
+
+output "intra_subnets" {
+  value = "${module.vpc.intra_subnets}"
+}
